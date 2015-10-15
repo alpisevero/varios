06c478c7fb8fb685a55085605edb4162d6c932a6
MAFIOSO <MAFIOSO@ASUS2012C>
2015-10-15 09:06:18 +0200

    INICIO 1

--- Proyectoalpha/src/proyectoalpha/Proyectoalpha.java
+++ Proyectoalpha/src/proyectoalpha/Proyectoalpha.java
@@ -1,17 +1,8 @@
 //Proyecto Alpha 1
 package proyectoalpha;
-
-/**
- *
- * @author SNOW
- */
 public class Proyectoalpha {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // 
+        
     }
     
 }
